e445b716957535ee02f46c5747d30d4e1a043f93
AshishK3197 <ashishk3197@gmail.com>
2020-12-27 00:15:28 +0530

    Implemented Api Call and loaded Response Data into Console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import Input from "./Components/Input";
+import axios from "./axios";
 
 function App() {
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get();
+			console.log(request);
+			return request;
+		}
+
+		fetchData();
+	}, []);
+
 	return (
 		<div className="App">
-			<h2>Convert Currency</h2>
+			<h1>Convert Currency</h1>
 			<Input className="App__input" />
 			<div className="App__equals">=</div>
 			<Input className="App__input" />
