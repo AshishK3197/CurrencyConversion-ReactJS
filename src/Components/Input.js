2dadf818ee7e7d5d6f36613d68b2537e6c4c51c6
AshishK3197 <ashishk3197@gmail.com>
2020-12-26 21:54:21 +0530

    Initial commit finalizing UI

---
+++ src/Components/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Input.css";
+
+function Input() {
+	return (
+		<div>
+			<input type="number" className="input" />
+			<select>
+				<option value="Hi">Hi</option>
+			</select>
+		</div>
+	);
+}
+
+export default Input;
